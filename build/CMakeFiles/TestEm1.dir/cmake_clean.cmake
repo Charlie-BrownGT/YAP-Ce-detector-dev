70e3ee66a42cabb221b08b7bc2bad9f44cdfd85f
Guy Carter-Richardson <Guy.Carter-Richardson@outlook.com>
2024-10-16 14:09:08 +0100

    update

---
+++ build/CMakeFiles/TestEm1.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestEm1.dir/TestEm1.cc.o"
+  "CMakeFiles/TestEm1.dir/TestEm1.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/TestEm1.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/TestEm1.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/EventAction.cc.o"
+  "CMakeFiles/TestEm1.dir/src/EventAction.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/HistoManager.cc.o"
+  "CMakeFiles/TestEm1.dir/src/HistoManager.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/PhysListEmStandard.cc.o"
+  "CMakeFiles/TestEm1.dir/src/PhysListEmStandard.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/TestEm1.dir/src/PhysicsList.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/TestEm1.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/Run.cc.o"
+  "CMakeFiles/TestEm1.dir/src/Run.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/RunAction.cc.o"
+  "CMakeFiles/TestEm1.dir/src/RunAction.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/StackingAction.cc.o"
+  "CMakeFiles/TestEm1.dir/src/StackingAction.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/StepMax.cc.o"
+  "CMakeFiles/TestEm1.dir/src/StepMax.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/TestEm1.dir/src/SteppingAction.cc.o.d"
+  "CMakeFiles/TestEm1.dir/src/TrackingAction.cc.o"
+  "CMakeFiles/TestEm1.dir/src/TrackingAction.cc.o.d"
+  "TestEm1"
+  "TestEm1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestEm1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
